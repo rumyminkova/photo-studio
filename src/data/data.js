d7596a0101d901db6d25f8855373cfae341ab12c
Rumyana Dimitrieva <rumyanamm@gmail.com>
2020-12-12 16:18:49 -0800

    Collection Fixes

--- src/data/data.js
+++ src/data/data.js
@@ -1,10 +1,10 @@
 export const COLLECTION_ITEMS = [
-  { id: 1, image: "images/nature.jpeg", text: "Nature" },
-  { id: 2, image: "images/wedding.jpeg", text: "Wedding" },
-  { id: 3, image: "images/party.jpeg", text: "Party" },
-  { id: 4, image: "images/business.jpeg", text: "Business" },
+  { id: 1, image: "images/nature.jpg", text: "Nature" },
+  { id: 2, image: "images/wedding.jpg", text: "Wedding" },
+  { id: 3, image: "images/party.jpg", text: "Party" },
+  { id: 4, image: "images/business.jpg", text: "Business" },
   { id: 5, image: "images/fashion.jpeg", text: "Fashion" },
-  { id: 6, image: "images/family.jpeg", text: "Family" },
+  { id: 6, image: "images/family.jpg", text: "Family" },
 ];
 
 export const CAROUSEL_ITEMS = [
